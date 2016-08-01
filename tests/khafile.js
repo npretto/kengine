7c49fb29299513e3a31107c5db75d17842ab11f7
BlackLanzer <blacklanzer@gmail.com>
2016-08-01 14:32:34 +0200

    utest missing from the khafile

--- tests/khafile.js
+++ tests/khafile.js
@@ -2,5 +2,6 @@ var project = new Project('New Project');
 project.addAssets('Assets/**');
 project.addSources('Sources');
 project.addSources('../');
+project.addLibrary('utest');
 resolve(project);
 return project;
