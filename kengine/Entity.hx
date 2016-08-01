b3229c7b84067c533cd3aa48554c99f0cc26a4f5
npretto <lordkryss@gmail.com>
2016-08-01 12:36:12 +0200

    added lost Entity file

---
+++ kengine/Entity.hx
@@ -0,0 +1,84 @@
+package kengine;
+import kengine.Component;
+import kha.Framebuffer;
+import kha.math.Vector2;
+
+/**
+ * ...
+ * @author npretto
+ */
+class Entity
+{
+	
+	var children:List<Entity> = new List();
+	var components : List<Component> = new List();
+	var parent:Entity = null;
+	var pos:Vector2 = new Vector2();
+	public var worldPos(get, null):Vector2;
+	public var rotation : Float; // degrees
+	
+	public var name:String = "";
+
+	public function new() 
+	{
+		
+	}
+	
+	function onAdded()
+	{
+		
+	}
+	
+	public function addChild(e:Entity):Entity
+	{
+		children.add(e);
+		e.parent = this;
+		e.onAdded();
+		return this;
+	}
+	
+	public function addComponent(c : Component):Entity
+	{
+		components.add(c);
+		c.owner = this;
+		c.onAdded();
+		return this;
+	}
+	
+	public function update()
+	{
+		for (c in components)
+		{
+			c.update();
+		}
+		
+		for (e in children)
+		{
+			e.update();
+		}
+	}
+	
+	public function render(framebuffer:Framebuffer)
+	{
+		for (c in components)
+		{
+			c.render(framebuffer);
+		}
+		for (e in children)
+		{
+			e.render(framebuffer);
+		}
+	}
+	
+		
+	private function get_worldPos():Vector2
+	{
+		if (parent == null)
+		{
+			return pos;
+		}else{
+			return parent.worldPos.add(pos);
+		}
+	}
+	
+}
