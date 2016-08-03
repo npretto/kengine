76cb5ac5b38a2421e1932d9b5a1bdb74a100fe0c
BlackLanzer <blacklanzer@gmail.com>
2016-08-03 17:12:24 +0200

    Fixed sprite rotation

--- kengine/graphics/ImageSprite.hx
+++ kengine/graphics/ImageSprite.hx
@@ -2,6 +2,7 @@ package kengine.graphics;
 
 import kengine.Component;
 import kengine.Entity;
+import kha.Color;
 import kha.Framebuffer;
 import kha.Image;
 import kha.Scaler;
@@ -25,15 +26,15 @@ class ImageSprite extends Drawable
 	override public function render(frameBuffer:Framebuffer) 
 	{
 		super.render(frameBuffer);
-
+		frameBuffer.g2.color = Color.White;
 		// reset the transformation matrix, that will be used by drawImage
-		//frameBuffer.g2.transformation = FastMatrix3.translation(offset.x, offset.y);
+		//frameBuffer.g2.transformation = FastMatrix3.translation(-offset.x, -offset.y);
 		frameBuffer.g2.transformation = FastMatrix3.translation(0, 0);
 		// rotation
-		frameBuffer.g2.rotate(MathTools.toRadians(owner.rotation), owner.worldPos.x + offset.x, owner.worldPos.y + offset.y);
+		frameBuffer.g2.rotate(MathTools.toRadians(owner.rotation), owner.worldPos.x, owner.worldPos.y);
 		
 		// draw the image scaled
-		frameBuffer.g2.drawScaledImage(image, owner.worldPos.x, owner.worldPos.y, size.x * scale.x, size.y * scale.y);
+		frameBuffer.g2.drawScaledImage(image, owner.worldPos.x - (offset.x * scale.x), owner.worldPos.y - (offset.y * scale.y), size.x * scale.x, size.y * scale.y);
 	}
 	
 	override public function get_size() : Vector2
